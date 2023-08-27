e00ef1cca31e30753e02add4d5a260f514b6e0e0
alexi <alexi@ALEXANDER>
2023-08-27 13:49:15 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Hello, World!");
+float celsius = 0.0f, fahrenheit = 0.0f;
+Console.WriteLine("Ingrese la temperatura en grados Fahrenheit:");
+fahrenheit = Convert.ToSingle(Console.ReadLine());
+
+celsius = (fahrenheit - 32) * 5 / 9;
+
+Console.WriteLine(fahrenheit + " grados Fahrenheit es equivalente a " + celsius + " grados celsius");
+
+Console.ReadKey();
